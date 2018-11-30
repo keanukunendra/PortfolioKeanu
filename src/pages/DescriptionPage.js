9884f7c2422d3610e0ef408c021ec0e6ec7373db
irsal firansyah <muhamadirsal@yahoo.com>
2018-11-30 14:56:26 +0700

    test description/page

---
+++ src/pages/DescriptionPage.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+
+const App = () => {
+  render () {
+    return (
+      <div>
+        <Navbar />
+        <Footer />
+      </div>
+    );
+  }
+}
+
+export default DescriptionPage;
