3a529fe75643a1161a802c66395096f086d89694
tetrapentana <tritetrapentana@gmail.com>
2018-12-04 21:22:41 +0700

    add contact pages

---
+++ src/pages/Contact.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+
+
+const Contact = () => {
+  return (
+    <div>
+      <Navbar />
+        <div className="img">
+            <h1 className="text-img">contact krama</h1>
+            <h2>We'd love to hear you shout</h2>
+        </div>
+        <div className="first-wrap">
+            <section id="form">
+            <div className="wrap2" >
+                <div className="title"> 
+                    <p className="detail">Connect with us:<br />For support or any questions <br /> Email us at <br /> <a className="link" href="https://binar.co.id/">krama-team@gmail.com</a> </p>
+                </div>
+                <div >
+                    <p className="detail2">Krama Yogyakarta<br />Jl. Damai No.89, Wonorejo, Sariharjo, Ngaglik, Kabupaten Sleman, Daerah Istimewa Yogyakarta 55581</p>
+                </div>
+                <div >
+                    <p className="detail3">Krama Batam<br />Jl. Hang Lekiu No.KM 2, Sambau, Nongsa, Kota Batam, Kepulauan Riau 29465</p>
+                </div>
+            </div>
+            </section>  
+        </div>
+        <div className="sec-wrap">
+            <div className="wrap-form">
+                <h1>Get in Touch</h1>
+                <h3>Please fill out the form and we will get connected</h3>
+                <form className="contact-form" action="">
+                    <div><input name="name" className="name" type="text" placeholder="Name" /></div>
+                    <div><input name="email" className="name" type="text" placeholder="You email address" /></div>
+                    <div><textarea name="message" name="" id="message" cols="50" rows="10" placeholder="Message"></textarea></div>
+                    <div><button className="btn-submit" type="submit" value="submit">submit</button></div>
+                </form> 
+            </div>      
+        </div>
+      <Footer />
+  </div>
+  )
+}
+
+export default Contact;
