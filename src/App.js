61cbf41e1b3970a9cda48db026bc34937ff91f08
irsal firansyah <muhamadirsal@yahoo.com>
2018-12-14 11:26:19 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,21 +1,9 @@
 import React from 'react';
-<<<<<<< HEAD
-import { BrowserRouter, Route } from 'react-router-dom';
-
-import DescriptionPage from './pages/DescriptionPage';
-import HomePage from './pages/HomePage';
-
-const App = () => { 
-  return (
-    <BrowserRouter>
-      <div className="App">
-        <Route path="/" component={HomePage} exact />
-        <Route path="/description" component={DescriptionPage} />
-=======
 import Navbar from './components/Navbar';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './pages/Home';
 import Aboutus from './pages/Aboutus';
+import DescriptionPage from './pages/DescriptionPage';
 
 const App = () => {
   return(
@@ -25,8 +13,8 @@ const App = () => {
         <Switch>
           <Route exact path='/' component={Home} />
           <Route path='/about' component={Aboutus} />
+          <Route path='/description' component={DescriptionPage} />
         </Switch>
->>>>>>> afca86df62761d3b9cd2d2c675368d5d7e11134b
       </div>
     </BrowserRouter>
   )
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,29 +1,16 @@
 import React from 'react';
-<<<<<<< HEAD
-import { NavLink } from 'react-router-dom';
-=======
 import { Link, NavLink } from 'react-router-dom';
->>>>>>> afca86df62761d3b9cd2d2c675368d5d7e11134b
 
 const Navbar = () => {
   return (
     <div className="section navbar shadow">
       <div className="flex flex-space-between">
         <div className="logo">
-<<<<<<< HEAD
-          <NavLink to="/">KRAMA</NavLink>
-        </div>
-        <div className="navigation flex flex-space-between">
-        <NavLink to="/" className="menu">Home</NavLink>
-          <a href="#About" className="menu">About Us</a>
-          <a href="#Contact" className="menu">Contact Us</a>
-=======
           <Link to='/'>KRAMA</Link>
         </div>
         <div className="navigation flex flex-space-between">
           <NavLink to='/' className="menu">Home</NavLink>
           <NavLink to='/about' className="menu">About</NavLink>
->>>>>>> afca86df62761d3b9cd2d2c675368d5d7e11134b
         </div>
         <div className="search">
           <i className='bx bx-search-alt bx-sm bx-tada'></i>
