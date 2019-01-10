55557b739725e875a15cfce26d86e65214dbd9b4
Yuhanas Yulianto <yulianto.yuhanas@gmail.com>
2019-01-10 08:04:12 +0700

    link card to description page

--- src/components/Catalogue.js
+++ src/components/Catalogue.js
@@ -1,30 +1,27 @@
-import React, { Component } from 'react';
-import Card from '../components/Card';
-import axios from 'axios';
+import React, { Component } from "react";
+import axios from "axios";
 
+import Card from "../components/Card";
+import { Link } from "react-router-dom";
 
 class Catalogue extends Component {
-  state = { 
+  state = {
     cards: []
-  }
-  
+  };
+
   // GET from API after all the DOM is rendered
   componentDidMount() {
-    console.log('component did mount');
-    console.log('-------------------')
-
     const category = this.props.match.params.path_id;
-    axios.get('https://staging-krama.herokuapp.com/api/v1/places')
-    .then(res => {
+    axios.get("https://staging-krama.herokuapp.com/api/v1/places").then(res => {
       this.setState(() => ({
-          id: category,
-          cards: res.data.slice(0, 8)
-        }))
-      });
+        id: category,
+        cards: res.data.slice(0, 8)
+      }));
+    });
   }
-    
-  render(){
-    console.log('render');
+
+  render() {
+    console.log("render");
     const { cards } = this.state;
 
     // render element using array
@@ -32,43 +29,42 @@ class Catalogue extends Component {
       cards.map(card => {
         return (
           // if response is OK and JSON is stored in this.state, which means length > 0
-          <Card title={card.title} desc={card.desc} key={card.id} />
-        )
+          <Link to="/description" key={card.id}>
+            <Card title={card.title} desc={card.desc} />
+          </Link>
+        );
       })
     ) : (
       // if response is error show this message instead
       <div>Loading Page...</div>
-    )
+    );
 
     return (
       <section id="catalogue">
-        <div className='section margin-top margin-bottom'>
+        <div className="section margin-top margin-bottom">
           <div className="container">
             <div>
-            <h2 className="f2 title">Destination</h2>
+              <h2 className="f2 title">Destination</h2>
             </div>
-            <div className="container flex flex-space-between">
-              {cardList}
-            </div>
+            <div className="container flex flex-space-between">{cardList}</div>
           </div>
         </div>
       </section>
-    )
+    );
   }
 
   componentDidUpdate(prevProps, prevState, snapshot) {
-    console.log('component did update');
-    console.log('-------------------')
     if (prevProps.match.params.path_id !== this.props.match.params.path_id) {
       const category = this.props.match.params.path_id;
-      axios.get('https://staging-krama.herokuapp.com/api/v1/' + category)
-      .then(res => {
-        this.setState({
-          id: category,
-          cards: res.data.slice(0, 8)
-        })
-        console.log(res)
-      })
+      axios
+        .get("https://staging-krama.herokuapp.com/api/v1/" + category)
+        .then(res => {
+          this.setState({
+            id: category,
+            cards: res.data.slice(0, 8)
+          });
+          console.log(res);
+        });
     }
   }
 }
