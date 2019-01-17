7433cd072e629b0f3d276f62ec8d688a637fe2ca
Yuhanas Yulianto <yulianto.yuhanas@gmail.com>
2019-01-17 10:18:20 +0700

    responsive Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,37 +1,47 @@
-import React from "react";
+import React, { Component } from "react";
 import { Link, NavLink } from "react-router-dom";
 
-const Navbar = () => {
-  return (
-    <div className="section navbar shadow">
-      <div class="nav">
-        <div class="nav-header">
-          <div class="nav-title">
+class Navbar extends Component {
+  state = {
+    isOpen: false
+  };
+
+  toggleMenu = () => {
+    const isOpen = !this.state.isOpen;
+    this.setState({
+      isOpen
+    });
+  };
+  render() {
+    return (
+      <div className="navbar shadow">
+        <div className="logo">
           <Link to="/">KRAMA</Link>
+          <div className="humberger" onClick={this.toggleMenu}>
+            <span />
+            <span />
+            <span />
           </div>
         </div>
-        <div class="nav-btn">
-          <label for="nav-check">
-            <span></span>
-            <span></span>
-            <span></span>
-          </label>
-        </div>
-        <input type="checkbox" id="nav-check"/>
-        <div class="nav-links ">
-          <NavLink to="/home" className="menu">
-          Home
+        <div
+          className={
+            "navigation flex flex-space-between " +
+            (this.state.isOpen ? null : "hidden")
+          }
+        >
+          <NavLink to="/home" className="menu" onClick={this.toggleMenu}>
+            Home
           </NavLink>
-          <NavLink to="/about" className="menu">
-          About Us
+          <NavLink to="/about" className="menu" onClick={this.toggleMenu}>
+            About
           </NavLink>
-          <NavLink to="/contact" className="menu">
-          Contact Us
+          <NavLink to="/contact" className="menu" onClick={this.toggleMenu}>
+            Contact
           </NavLink>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Navbar;
